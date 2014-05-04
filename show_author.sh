ca1fdd9554dc7f61168d34875d5c7873a7a0526e
Josua Krause <josua.krause@gmail.com>
2014-05-04 10:03:46 -0400

    show author edits

---
+++ show_author.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [[ -z $1 ]]; then
+    echo "usage: $0 <author>"
+    exit 1
+fi
+
+test=`ls -A "authors_pages/$1" | grep "$1"`
+
+if [[ -z "$test" ]]; then
+    echo "user $1 does not exist"
+    exit 1
+fi
+
+function author() {
+    echo "Author: $1"
+    echo "Page: http://en.wikipedia.org/wiki/User:$1"
+    echo "Edits:"
+    cat "authors_pages/$1"
+}
+
+author "$1" | less

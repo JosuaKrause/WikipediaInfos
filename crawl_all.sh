e6bac6220e93674ae69d59c233731b5e091583dc
Josua Krause <josua.krause@gmail.com>
2014-05-13 20:01:13 -0400

    crawl depth 2

--- crawl_all.sh
+++ crawl_all.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-./crawl_authors.sh Category:Social_sciences Category:Social_sciences 1
+./crawl_authors.sh Category:Social_sciences Category:Social_sciences 2
 ./compact.sh Social_sciences
-./crawl_authors.sh Category:Computer_science Category:Computer_science 1
+./crawl_authors.sh Category:Computer_science Category:Computer_science 2
 ./compact.sh Computer_science
 #./crawl_authors.sh Category:Information_technology Category:Information_technology 1
-./crawl_authors.sh Category:Arts Category:Arts 1
+./crawl_authors.sh Category:Arts Category:Arts 2
 ./compact.sh Arts
-./crawl_authors.sh Category:Mathematics Category:Mathematics 1
+./crawl_authors.sh Category:Mathematics Category:Mathematics 2
 ./compact.sh Mathematics
-./crawl_authors.sh Category:Cryptography Category:Cryptography 1
+./crawl_authors.sh Category:Cryptography Category:Cryptography 2
 ./compact.sh Cryptography

b8a0294de7c3692edc77db9e69226661f31b690f
Josua Krause <josua.krause@gmail.com>
2014-05-16 11:40:36 -0400

    dumbed down find_authors

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Wikipedia - Infos
 
 ./show_intersections.sh -a "Category:Computer_science" "Category:Mathematics" | grep -v bot | ./get_authors_count.sh | sort -gr | less
 
-./find_authors.sh 0 "bot" | ./get_authors_count.sh | sort -gr | less
+./find_authors.sh | grep -v bot | ./get_authors_count.sh | sort -gr | less
 
 ./show_author_count.sh 1 "bot" | less
 
 ./show_author_count.sh -m 0 "bot"
 
-./find_authors.sh 0 | ./get_authors_count.sh | sort -gr | less
+./find_authors.sh | ./get_authors_count.sh | sort -gr | less
 
 ./show_author.sh "namehere"
 
--- find_authors.sh
+++ find_authors.sh
@@ -1,27 +1,3 @@
 #!/bin/sh
 
-if [[ -z "$1" ]]; then
-    echo "Usage: $0 <use_stdin> [<filter>]"
-fi
-
-if [[ -z $2 ]]; then
-    filter=
-else
-    filter=$2
-fi
-
-if [[ "$1" == "1" ]]; then
-    while read line; do
-        fil=`echo "$f" | grep "$filter"`
-        if [ -z "$filter" ] || [ -z "$fil" ]; then
-            echo "$line"
-        fi
-    done < /dev/stdin
-else
-    for line in `ls -A authors_pages`; do
-        fil=`echo "$line" | grep "$filter"`
-        if [ -z "$filter" ] || [ -z "$fil" ]; then
-            echo "$line"
-        fi
-    done
-fi
+ls -A authors_pages

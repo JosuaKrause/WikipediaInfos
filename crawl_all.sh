cdc6bf3c5cb005c45ae1f6cf6d5e59109726fa72
Josua Krause <josua.krause@gmail.com>
2014-05-15 22:07:46 -0400

    compress

--- crawl_all.sh
+++ crawl_all.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 ./crawl_authors.sh "Category:Social_sciences" "Category:Social_sciences" 2
-#./compact.sh Social_sciences
+./compact.sh Social_sciences
 ./crawl_authors.sh "Category:Computer_science" "Category:Computer_science" 2
-#./compact.sh Computer_science
+./compact.sh Computer_science
 #./crawl_authors.sh Category:Information_technology Category:Information_technology 1
 ./crawl_authors.sh "Category:Arts" "Category:Arts" 2
-#./compact.sh Arts
+./compact.sh Arts
 ./crawl_authors.sh "Category:Mathematics" "Category:Mathematics" 2
-#./compact.sh Mathematics
+./compact.sh Mathematics
 ./crawl_authors.sh "Category:Cryptography" "Category:Cryptography" 2
-#./compact.sh Cryptography
+./compact.sh Cryptography

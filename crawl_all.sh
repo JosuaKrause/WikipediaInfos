99e64b4e70625c1ddd01459bb9f897971f3b4e2f
Josua Krause <josua.krause@gmail.com>
2014-05-15 22:15:21 -0400

    reduced size

--- crawl_all.sh
+++ crawl_all.sh
@@ -5,16 +5,16 @@
 ./crawl_authors.sh "Category:Computer_science" "Category:Computer_science" 2
 ./compact.sh Computer_science
 #./crawl_authors.sh Category:Information_technology Category:Information_technology 1
-./crawl_authors.sh "Category:Arts" "Category:Arts" 2
-./compact.sh Arts
+#./crawl_authors.sh "Category:Arts" "Category:Arts" 2
+#./compact.sh Arts
 ./crawl_authors.sh "Category:Mathematics" "Category:Mathematics" 2
 ./compact.sh Mathematics
-./crawl_authors.sh "Category:Cryptography" "Category:Cryptography" 2
-./compact.sh Cryptography
+#./crawl_authors.sh "Category:Cryptography" "Category:Cryptography" 2
+#./compact.sh Cryptography
 
 ./create_backlinks.sh
 ./show_cat.sh "Category:Social_sciences"
 ./show_cat.sh "Category:Computer_science"
-./show_cat.sh "Category:Arts"
+#./show_cat.sh "Category:Arts"
 ./show_cat.sh "Category:Mathematics"
-./show_cat.sh "Category:Cryptography"
+#./show_cat.sh "Category:Cryptography"
